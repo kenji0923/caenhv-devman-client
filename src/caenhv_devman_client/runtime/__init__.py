2f219aeabb72c64246350d9b0cf15b30bd5ea82c
Kenji Shu <kenjishu@fas.harvard.edu>
2026-03-03 00:31:08 -0500

    Remove server related files

--- src/caenhv_devman_client/runtime/__init__.py
+++ src/caenhv_devman_client/runtime/__init__.py
@@ -1,11 +1,6 @@
 from .client import ManagerClient, ManagerError
-from .db import OwnershipDB
-from .server import RuntimeFunctionSpec, serve_manager
 
 __all__ = [
     "ManagerClient",
     "ManagerError",
-    "OwnershipDB",
-    "RuntimeFunctionSpec",
-    "serve_manager",
 ]
